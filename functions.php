33c90ffc3c09fcf087c22540c9e8af6ab30538ae
nakaken88 <nakaken88888888@gmail.com>
2018-11-24 15:22:28 +0900

    add Content column to All Posts Screen in admin

--- functions.php
+++ functions.php
@@ -1,5 +1,39 @@
 <?php
 
+// add Content column to All Posts Screen in admin
+function add_post_columns( $columns ) {
+
+    $new_columns = array();
+
+    foreach ( $columns as $column_name => $column_display_name ) {
+        if ( $column_name == 'comments' ) {
+            $new_columns['content'] = __( 'Content' );
+        }
+        $new_columns[ $column_name ] = $column_display_name;
+    }
+
+    return $new_columns;
+}
+add_filter( 'manage_posts_columns' , 'add_post_columns' );
+
+function custom_columns( $column, $post_id ) {
+    switch ( $column ) {
+        case 'content' :
+            $max_length = 100;
+            $my_content = get_the_content();
+            $my_content = wp_strip_all_tags( $my_content );
+
+            if ( mb_strlen( $my_content ) > $max_length ) {
+                $my_content = mb_substr( $my_content, 0, $max_length ) . ' [...]';
+            }
+            echo $my_content;
+            break;
+    }
+}
+add_action( 'manage_posts_custom_column' , 'custom_columns', 10, 2 );
+
+
+// setting to insert tag to head in customizer
 function mytheme_customize_register( $wp_customize ) {
 
     $wp_customize->add_section( 'other_options', array(
